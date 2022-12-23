0851a4e7dd5e5f79b4006949e9f130583519a64b
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-23 22:08:24 +0200

    Redux

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+
+  return <div>Counter</div>;
+};
+
+export default Counter;
