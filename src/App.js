e4341de176d7d075e6926f0a20d93f679b573919
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-24 20:56:24 +0200

    Adding a folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
-import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Lesson 2</h1>
-      <AddPostForm />
-      <PostsList />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
