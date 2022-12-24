36200b156f6486859c3a836d7e3a756d834d3927
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-24 18:15:27 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
   return (
     <div className="App">
       <h1>Lesson 2</h1>
+      <AddPostForm />
+      <PostsList />
     </div>
   );
 }
