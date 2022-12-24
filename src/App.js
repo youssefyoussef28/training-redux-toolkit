0ec961975b076a055910f1f4d88731b8b7be0dcf
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-24 13:38:08 +0200

    Lesson one Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
