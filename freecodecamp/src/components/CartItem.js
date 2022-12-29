1ef2c2f221979ab6a421a4a04647a6f100ebb6d0
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-29 13:42:20 +0200

    setting up cart

--- freecodecamp/src/components/CartItem.js
+++ freecodecamp/src/components/CartItem.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { ChevronDown, ChevronUp } from "../icons";
 
-const CartItem = () => {
-  return <div>Cart Item</div>;
+const CartItem = ({ id, img, title, price, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">Remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <div>
+          <p className="amount">{amount}</p>
+          <button className="amount-btn">
+            <ChevronDown />
+          </button>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default CartItem;
